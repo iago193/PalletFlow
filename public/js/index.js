f97c561ee553f70ac546f8beaf90ae70655e6b54
@iago193 <iago.silva6969@gmail.com>
2025-06-10 12:37:09 -0300

    +

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+
+function abrirModalCarregar() {
+    let ModalCarregar = document.getElementById('container-model');
+    ModalCarregar.style.display = 'block';
+}
+
+function fecharModalCarregar() {
+    let ModalCarregar = document.getElementById('container-model');
+    ModalCarregar.style.display = 'none';
+}
